05ca42e059da457944c7d42c6e08040eaa9d527d
pjialong <p2358091062>
2023-12-12 14:48:53 +0800

    调整

--- src/main/resources/security/MySecurityManager.java
+++ src/main/resources/security/MySecurityManager.java
@@ -23,7 +23,7 @@ public class MySecurityManager extends SecurityManager {
         // if (file.contains("D:\\project\\yuoj-code-sandbox")) {
         //     return;
         // }
-//        throw new SecurityException("checkRead 权限异常：" + file);
+       throw new SecurityException("checkRead 权限异常：" + file);
     }
 
     // 检测程序是否允许写文件
@@ -41,6 +41,6 @@ public class MySecurityManager extends SecurityManager {
     // 检测程序是否允许连接网络
     @Override
     public void checkConnect(String host, int port) {
-//        throw new SecurityException("checkConnect 权限异常：" + host + ":" + port);
+       throw new SecurityException("checkConnect 权限异常：" + host + ":" + port);
     }
 }
